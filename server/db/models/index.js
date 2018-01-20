cc2eb6c93eddb069cf2e7d74d2b9fbf7c859be02
Abel McElroy <abel.h.mcelroy@gmail.com>
2018-01-20 13:45:08 -0500

    new branch for debugging TS alg

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./user')
+const History = require('./history')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -14,5 +15,6 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  History
 }
