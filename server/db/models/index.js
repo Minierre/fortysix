a2ff06b393efa45a93f6e8339135f3c80ccbbb35
danielbh <danielhollcraft@gmail.com>
2018-01-26 15:38:45 -0500

    seed

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,11 +3,13 @@ const History = require('./history')
 const Room = require('./room');
 const Mutations = require('./mutations')
 const Selections = require('./selections')
+const Fitness = require('./fitness')
 
 module.exports = {
   User,
   History,
   Room,
   Mutations,
-  Selections
+  Selections,
+  Fitness
 }
