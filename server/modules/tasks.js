0c4c48c22d4e95ac0b01d2a15f1869b34f081c30
danielbh <danielhollcraft@gmail.com>
2018-01-27 20:07:18 -0500

    cleaning up

--- server/modules/tasks.js
+++ server/modules/tasks.js
@@ -1,36 +1,22 @@
 const uuid = require('uuid/v1')
-const { Mutations, Selections, Fitness } = require('../db/models')
-// Generate task objects based on parameters input
 
-function generateTasks({ params }, room, numTasks, fitness, mutations, selection, chromosomeLength) {
+function generateTasks(
+  populationSize,
+  room,
+  numTasks,
+  fitness,
+  mutations,
+  selection,
+  chromosomeLength
+) {
   const tasks = []
-
-  // const mutations = await Mutations.findAll({
-  //   where: {
-  //     id: mutids
-  //   },
-  //   attributes: ['function']
-  // });
-
-
-  // const mutations = await Mutations.findById(params.currentMutationFunc, {
-  //   attributes: ['function']
-  // });
-
-  // const selection = await Selections.findById(params.currentSelectionFunc,
-  //   { attributes: ['function']
-  // })
-
-  // const fitness = await Fitness.findById(params.fitnessFunc,
-  //   { attributes: ['function']
-  // })
 
   for (let i = 0; i < numTasks; i++) {
     const task = {
       room,
       id: uuid(),
       gen: 1,
-      population: genPop(chromosomeLength, params.population),
+      population: genPop(chromosomeLength, populationSize),
       fitness,
       mutations: [mutations],
       selection,
@@ -48,43 +34,5 @@ function genPop(l, s) {
   }
   return pop
 }
-
-// , ((portions) => {
-//   rooms[room].tasks = (rooms[room].multiThreaded) ?
-//     portions.reduce((a, b, i) => {
-//       if (a[Math.floor(i / 4)]) a[Math.floor(i / 4)].value.push(b.value[0])
-//       else a[Math.floor(i / 4)] = b
-//       return a
-//     }, []) :
-//     portions.map(v => ({
-//       id: v.id,
-//       value: v.value[0]
-//     }))
-
-/*
-this would be the more optimized approach with the task queue, but for now
-i'll just unshift the task queue:
-
-keep the task queue one long array and trim it as it gets too big
-
-function getTask(taskQ) {
-  let taskNum = 0;
-  return function wrapped() {
-    const task = taskQ[taskNum];
-    taskNum++;
-    return task;
-  }
-}
-
-function garbageCollection(taskNum){
-  if (taskNum === 100) {
-    task = task.slice(100, taskQ.length -1)
-    taskNum = 0
-  }
-}
-*/
-
-// shortens the taskQueue when it
-
 
 module.exports = { generateTasks }
