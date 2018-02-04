07c805effc63a79f3604218e3179e640390e39be
danielbh <danielhollcraft@gmail.com>
2018-02-04 17:23:29 -0500

    hotfix: css scientist form

--- client/components/AdminInputs/index.js
+++ client/components/AdminInputs/index.js
@@ -43,7 +43,7 @@ class AdminInputs extends Component {
           <h3>Parameters</h3>
           <div className="sliders">
             <Form>
-              <Col sm={3}>
+              <Col sm={6} md={3} xs={12}>
                 <Input
                   controlId="populationSize"
                   label="Population Size"
@@ -63,7 +63,7 @@ class AdminInputs extends Component {
                   onChange={this.props.onChange}
                 />
               </Col>
-              <Col sm={3}>
+              <Col sm={6} md={3} xs={12}>
                 <Input
                   controlId="chromosomeLength"
                   label="Chromosome Length"
@@ -83,7 +83,7 @@ class AdminInputs extends Component {
                   onChange={this.props.onChange}
                 />
               </Col>
-              <Col sm={3}>
+              <Col sm={6} md={3} xs={12}>
                 <Input
                   controlId="fitnessGoal"
                   label="Fitness Goal"
@@ -103,7 +103,7 @@ class AdminInputs extends Component {
                   onChange={this.props.onChange}
                 />
               </Col>
-              <Col sm={3}>
+              <Col sm={6} md={3} xs={12}>
                 <Input
                   controlId="reproductiveCoefficient"
                   label="Reproductive Coefficient"
