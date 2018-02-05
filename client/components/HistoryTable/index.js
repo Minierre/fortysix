aa861e788301079762c32f632dcfe59caa6063a9
danielbh <danielhollcraft@gmail.com>
2018-02-04 20:41:01 -0500

    fancy history table now fully replaces old table.

--- client/components/HistoryTable/index.js
+++ client/components/HistoryTable/index.js
@@ -1,39 +1,64 @@
 import React from 'react'
 import {
-  Table
+  BootstrapTable,
+  TableHeaderColumn
+} from 'react-bootstrap-table'
+import {
+  Button
 } from 'react-bootstrap'
 import moment from 'moment'
 import './style.css'
 
-const HistoryTable = ({ data }) => (
-  <div className="history-table">
-    <Table striped responsive>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Date Run</th>
-          <th># of Nodes</th>
-          <th>Execution Time (seconds)</th>
-          <th>Result</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data && data.map(row => {
-          var date = new Date(row.startTime);
-          return (
-            <tr key={row.id}>
-              <td>{row.id}</td>
-              <td>{moment(date).format('MMMM Do YYYY, h:mm:ss a')}</td>
-              <td>{row.nodes}</td>
-              <td>{(new Date(row.endTime) - new Date(row.startTime)) / 1000}</td>
-              <td>{row.result}</td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </Table>
-  </div>
-
+const actionFormatter = (cell, row) => (
+  <Button
+    bsStyle="info"
+    onClick={() => console.log('open modal')}
+  >
+    More
+  </Button>
 )
+
+
+const HistoryTable = ({ data }) => {
+  const newData = data && data.reduce((acc, row) => {
+    const date = new Date(row.startTime)
+    return acc.concat({
+      ...row,
+      date: moment(date).format('MMMM Do YYYY, h:mm:ss a'),
+      execTime: (new Date(row.endTime) - new Date(row.startTime)) / 1000
+    })
+  }, [])
+
+  const selectRowProp = {
+    mode: 'checkbox',
+    bgColor: 'pink'
+  }
+
+  return (
+    <div className="history-table">
+      <BootstrapTable
+        data={newData}
+        selectRow={selectRowProp}
+        exportCSV
+        striped
+        hover
+        selectable
+      >
+        <TableHeaderColumn dataField="id" isKey hidden export>ID</TableHeaderColumn>
+        <TableHeaderColumn dataField="nodes"># of Nodes</TableHeaderColumn>
+        <TableHeaderColumn dataField="date">Date Run</TableHeaderColumn>
+        <TableHeaderColumn dataField="execTime">Execution Time (seconds)</TableHeaderColumn>
+        <TableHeaderColumn dataField="result">Result</TableHeaderColumn>
+        <TableHeaderColumn
+          dataField="action"
+          export={false}
+          dataFormat={actionFormatter}
+        >
+          Action
+        </TableHeaderColumn>
+      </BootstrapTable>
+    </div>
+  )
+}
 
 export default HistoryTable
