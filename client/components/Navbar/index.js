49fd11fd30f46f076f6d9b506bccc288a3bdea4d
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2018-02-07 14:51:39 -0500

    a user can logout

--- client/components/Navbar/index.js
+++ client/components/Navbar/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { logout } from '../../store'
 import {
   Navbar,
   Nav,
@@ -9,34 +11,52 @@ import {
 
 import './style.css'
 
-const NavigationBar = () => (
-  <Navbar inverse collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <Link to="/">FortySix</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav pullRight>
-        <LinkContainer to="/rooms">
-          <NavItem>
-            Rooms
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to="/asds">
-          <NavItem>
-            Top Contributors
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to="/login">
-          <NavItem>
-            Login
-          </NavItem>
-        </LinkContainer>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-)
+const NavigationBar = (props) => {
+  const logInOrOut = props.isLoggedIn ? 'logout' : 'login'
+  return (
+    <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link to="/">FortySix</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight>
+          <LinkContainer to="/rooms">
+            <NavItem>
+              Rooms
+            </NavItem>
+          </LinkContainer>
+          <LinkContainer to="/asds">
+            <NavItem>
+              Top Contributors
+            </NavItem>
+          </LinkContainer>
+          <LinkContainer to={logInOrOut}>
+            {props.isLoggedIn ?
+              <NavItem onClick={props.onLogout}>Logout</NavItem> :
+              <NavItem>Login</NavItem>}
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
 
-export default NavigationBar
+const mapState = (state) => {
+  return {
+    isLoggedIn: !!state.user.id,
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    onLogout(evt) {
+      evt.preventDefault()
+      dispatch(logout())
+    }
+  }
+}
+
+export default connect(mapState, mapDispatch)(NavigationBar)
